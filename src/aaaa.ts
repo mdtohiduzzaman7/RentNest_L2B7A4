0ca0c68b408d4b22a2b63c475add446c2e31e789
Md.Tohiduzzaman <mdtohiduzzaman7@gmail.com>
2026-07-24 22:52:13 +0600

    section new

--- src/aaaa.ts
+++ src/aaaa.ts
@@ -1,14 +1,3 @@
-sgdhgdfjhfgjfgj
-sfhsaddfjasdlf; jadslf
+import { getMyPayments } from "./controllers/payment.controller";
 
-what is the purpose of this
-code ?
-    The code snippet 
-    you provided appears to be a random string of characters and does not serve 
-any functional purpose
-git in a TypeScript or JavaScript contex
-
-t.It seems to be nonsensical and does not contain any v
-alid syntax or logic that would be executed by a TypeScript compiler or JavaScript engine.
-what is the purpose
- of this code ?
+getMyPayments
